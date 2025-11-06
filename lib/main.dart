22b5a1b5e0b6a5ce37ed588e7b139feebae8dd9b
Jaketh444 <up2274964@myport.ac.uk>
2025-11-06 19:27:34 +0000

    Implement counter functionality with setState

--- lib/main.dart
+++ lib/main.dart
@@ -79,12 +79,12 @@ void _decreaseQuantity() {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: () => print('Add button pressed!'),
-                child: const Text('Add'),
+              onPressed: _increaseQuantity,
+              child: const Text('Add'),
               ),
               ElevatedButton(
-                onPressed: () => print('Remove button pressed!'),
-                child: const Text('Remove'),
+              onPressed: _decreaseQuantity,
+              child: const Text('Remove'),
               ),
             ],
           ),
